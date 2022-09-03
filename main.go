73aaa9fcbb8ef3df2d908d29b70ccc679837cdea
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-03 10:22:13 +0700

    initialize and implement different routing and handle it

--- main.go
+++ main.go
@@ -10,8 +10,31 @@ func main() {
 	mux := http.NewServeMux()
 
 	//* Mendeklarasikan fungsi untuk http request dengan parameter pertama endpoint, parameter kedua handler function
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		if(req.URL.Path != "/") { //* Jika request endpoint berbeda dengan endpoint yang telah dideklarasikan
+			errorResponse(res, req, http.StatusNotFound)
+			return
+		}
+
+		res.Write([]byte("Hello world!"))
+	})
+
 	mux.HandleFunc("/api", func(res http.ResponseWriter, req *http.Request) {
+		if(req.URL.Path != "/api") {
+			errorResponse(res, req, http.StatusNotFound)
+			return
+		}
+
 		res.Write([]byte("Hello! this is an implement of golang for web development"))
+	})
+
+	mux.HandleFunc("/api/plants", func(res http.ResponseWriter, req *http.Request) {
+		if(req.URL.Path != "/api/plants") {
+			errorResponse(res, req, http.StatusNotFound)
+			return
+		}
+
+		res.Write([]byte("Plant list:\n1.Apple\n2.Strawberry"))
 	})
 
 	log.Println("Server connected at http://localhost:5000")
@@ -20,4 +43,12 @@ func main() {
 	error := http.ListenAndServe(":5000", mux) 
 	
 	log.Fatal(error) //* Untuk mencetak error yang terjadi
+}
+
+func errorResponse(res http.ResponseWriter, req *http.Request, status int){
+	res.WriteHeader(status)
+
+	if status == http.StatusNotFound {
+		res.Write([]byte("Data not found"))
+	}
 }
