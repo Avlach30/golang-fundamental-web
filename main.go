b3ee055cb08ee521bff2e60af9e4c6f2523d6a21
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-03 09:34:35 +0700

    refactor golang main function for implement web server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello!");
+	//* Menginisiasikan http handler untuk golang
+	mux := http.NewServeMux()
+
+	//* Mendeklarasikan fungsi untuk http request dengan parameter pertama endpoint, parameter kedua handler function
+	mux.HandleFunc("/api", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("Hello! this is an implement of golang for web development"))
+	})
+
+	log.Println("Server connected at http://localhost:5000")
+
+	//* Menjalankan server dengan port 5000 dengan http handler nya
+	error := http.ListenAndServe(":5000", mux) 
+	
+	log.Fatal(error) //* Untuk mencetak error yang terjadi
 }
