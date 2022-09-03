18f19557364f73fdf254a4c8abdacf285c0f8c1c
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-03 11:09:21 +0700

    define request query parameters from client request

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -12,7 +14,7 @@ func main() {
 	//* Mendeklarasikan fungsi untuk http request dengan parameter pertama endpoint, parameter kedua handler function
 	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		if(req.URL.Path != "/") { //* Jika request endpoint berbeda dengan endpoint yang telah dideklarasikan
-			errorResponse(res, req, http.StatusNotFound)
+			errorResponse(res, req, http.StatusNotFound, "Data not found")
 			return
 		}
 
@@ -21,7 +23,7 @@ func main() {
 
 	mux.HandleFunc("/api", func(res http.ResponseWriter, req *http.Request) {
 		if(req.URL.Path != "/api") {
-			errorResponse(res, req, http.StatusNotFound)
+			errorResponse(res, req, http.StatusNotFound, "Data not found")
 			return
 		}
 
@@ -30,11 +32,31 @@ func main() {
 
 	mux.HandleFunc("/api/plants", func(res http.ResponseWriter, req *http.Request) {
 		if(req.URL.Path != "/api/plants") {
-			errorResponse(res, req, http.StatusNotFound)
+			errorResponse(res, req, http.StatusNotFound, "Data not found")
 			return
 		}
 
-		res.Write([]byte("Plant list:\n1.Apple\n2.Strawberry"))
+		id := req.URL.Query().Get("id") //* Get query parameter 'id'
+
+		//* If id query parameter is undefined
+		if (id == "") {
+			res.Write([]byte("Plant list:\n1.Apple\n2.Strawberry"))
+			return
+		} 
+
+		idNum, error := strconv.Atoi(id) //*Convert string to integers
+
+		if (error != nil || idNum < 1) {
+			errorResponse(res, req, http.StatusBadRequest, "Sorry, only integers can be accepted as query params (min 1)")
+			return
+		}
+
+		//* If id query parameter is defined
+		if (idNum >= 1) {
+			fmt.Fprintf(res, "Plants id: %d", idNum)
+			return
+		}
+
 	})
 
 	log.Println("Server connected at http://localhost:5000")
@@ -45,10 +67,14 @@ func main() {
 	log.Fatal(error) //* Untuk mencetak error yang terjadi
 }
 
-func errorResponse(res http.ResponseWriter, req *http.Request, status int){
+func errorResponse(res http.ResponseWriter, req *http.Request, status int, message string){
 	res.WriteHeader(status)
 
-	if status == http.StatusNotFound {
-		res.Write([]byte("Data not found"))
+	if status == http.StatusNotFound { //* Handler for not found error
+		res.Write([]byte(message))
+	}
+
+	if status == http.StatusBadRequest { //* Handler for bad request error 
+		res.Write([]byte(message))
 	}
 }
