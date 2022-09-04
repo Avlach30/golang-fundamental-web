2f58772acce4e8d959dc5319482e01e667a9c975
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-04 10:47:56 +0700

    define handler with dynamic value for response render template views

--- handler/handler.go
+++ handler/handler.go
@@ -8,14 +8,14 @@ import (
 	"strconv"
 )
 
-func errorResponse(res http.ResponseWriter, req *http.Request, status int, message string){
+func errorResponse(res http.ResponseWriter, req *http.Request, status int, message string) {
 	res.WriteHeader(status)
 
 	if status == http.StatusNotFound { //* Handler for not found error
 		res.Write([]byte(message))
 	}
 
-	if status == http.StatusBadRequest { //* Handler for bad request error 
+	if status == http.StatusBadRequest { //* Handler for bad request error
 		res.Write([]byte(message))
 	}
 
@@ -25,59 +25,83 @@ func errorResponse(res http.ResponseWriter, req *http.Request, status int, messa
 }
 
 func RootHandler(res http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/" { //* Jika request endpoint berbeda dengan endpoint yang telah dideklarasikan
+	if req.URL.Path == "/" { 
+		errorResponse(res, req, http.StatusNotFound, "Data not found")
+		return
+	}
+}
+
+func RootApiHandler(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/api" { //* Jika request endpoint berbeda dengan endpoint yang telah dideklarasikan
 		errorResponse(res, req, http.StatusNotFound, "Data not found")
 		return
 	}
 
 	//* Memparsing file index.html sebagai template views
 	temp, err := template.ParseFiles(path.Join("views", "index.html"))
-	if (err != nil) {
+	if err != nil {
 		errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
 		return
 	}
 
 	err = temp.Execute(res, nil) //* Mengeksekusi temp variabel supaya bisa dijadikan response render
-	if (err != nil) {
+	if err != nil {
 		errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
 		return
 	}
-	// res.Write([]byte("Hello world!"))
-}
-
-func RootApiHandler(res http.ResponseWriter, req *http.Request) {
-	if(req.URL.Path != "/api") {
-		errorResponse(res, req, http.StatusNotFound, "Data not found")
-		return
-	}
-
-	res.Write([]byte("Hello! this is an implement of golang for web development"))
 }
 
 func GetPlantsHandler(res http.ResponseWriter, req *http.Request) {
-	if(req.URL.Path != "/api/plants") {
+	if req.URL.Path != "/api/plants" {
 		errorResponse(res, req, http.StatusNotFound, "Data not found")
 		return
 	}
 
 	id := req.URL.Query().Get("id") //* Get query parameter 'id'
 
-	//* If id query parameter is undefined
-	if (id == "") {
-		res.Write([]byte("Plant list:\n1.Apple\n2.Strawberry"))
+	temp, err := template.ParseFiles(path.Join("views", "plants.html"))
+	if err != nil {
+		errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
 		return
-	} 
+	}
+
+	//* If id query parameter is undefined
+	if id == "" {
+		//* Mendeklarasikan data untuk ditampilkan ke html template dengan nilai bebas
+		renderData := map[string]interface{}{
+			"title": "Plants page",
+			"content": "Plant list:\n1.Apple\n2.Strawberry",
+		}
+
+		//* Mengeksekusi temp variabel supaya bisa dijadikan response render dengan nilai dinamis
+		err = temp.Execute(res, renderData) 
+		if err != nil {
+			errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
+			return
+		}
+		return
+	}
 
 	idNum, error := strconv.Atoi(id) //*Convert string to integers
 
-	if (error != nil || idNum < 1) {
+	if error != nil || idNum < 1 {
 		errorResponse(res, req, http.StatusBadRequest, "Sorry, only integers can be accepted as query params (min 1)")
 		return
 	}
 
 	//* If id query parameter is defined
-	if (idNum >= 1) {
-		fmt.Fprintf(res, "Plants id: %d", idNum)
+	if idNum >= 1 {
+		renderData := map[string]interface{}{
+			"title": "Plants page",
+			"content": fmt.Sprintf("Plants id: %d", idNum),
+		}
+
+		err = temp.Execute(res, renderData) 
+		if err != nil {
+			errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
+			return
+		}
+
 		return
 	}
 
