61ab1ff83db655b3ca3c9871cd14069732344d35
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-09-07 20:57:29 +0700

    update handler of processing data from inputted form

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,9 @@
 package handler
 
 import (
-	"fundamental-webdev/entity"
 	"html/template"
 	"net/http"
 	"path"
-	"strconv"
 )
 
 func errorResponse(res http.ResponseWriter, req *http.Request, status int, message string) {
@@ -51,81 +49,6 @@ func RootApiHandler(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func GetPlantsHandler(res http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/api/plants" {
-		errorResponse(res, req, http.StatusNotFound, "Data not found")
-		return
-	}
-
-	id := req.URL.Query().Get("id") //* Get query parameter 'id'
-
-	temp, err := template.ParseFiles(path.Join("views", "plants.html"))
-	if err != nil {
-		errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
-		return
-	}
-
-	//* If id query parameter is undefined
-	if id == "" {
-
-		// renderData := map[string]interface{}{
-		// 	"title": "Plants page",
-		// 	"content": "Plant list:\n1.Apple\n2.Strawberry",
-		// }
-
-		//* Mendeklarasikan data dengan tipe data array of object untuk ditampilkan ke template
-		renderData := []entity.Plant{
-			{ID: 1, Name: "padi", Description: "penghasil beras"},
-			{ID: 2, Name: "cabai", Description: "penghasil rempah terbaik"},
-			{ID: 3, Name: "kentang", Description: "penghasil karbohidrat"},
-		}
-
-		//* Mengeksekusi temp variabel supaya bisa dijadikan response render dengan nilai dinamis
-		err = temp.Execute(res, renderData)
-		if err != nil {
-			errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
-			return
-		}
-		return
-	}
-
-	idNum, error := strconv.Atoi(id) //*Convert string to integers
-
-	if error != nil || idNum < 1 {
-		errorResponse(res, req, http.StatusBadRequest, "Sorry, only integers can be accepted as query params (min 1)")
-		return
-	}
-
-	//* If id query parameter is defined
-	if idNum >= 1 {
-		renderData := entity.Plant{
-			ID:          idNum,
-			Name:        "padi",
-			Description: "penghasil beras",
-		}
-
-		// renderData := map[string]interface{}{
-		// 	"title": "Plants page",
-		// 	"content": fmt.Sprintf("Plants id: %d", idNum),
-		// }
-
-		temp, err := template.ParseFiles(path.Join("views", "plant.html"))
-		if err != nil {
-			errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
-			return
-		}
-
-		err = temp.Execute(res, renderData)
-		if err != nil {
-			errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
-			return
-		}
-
-		return
-	}
-
-}
-
 func FetchHTTPMethod(res http.ResponseWriter, req *http.Request) {
 	METHOD := req.Method //* Fetch HTTP request method from client
 
@@ -166,12 +89,30 @@ func ProcessAddPlantForm(res http.ResponseWriter, req *http.Request) {
 			errorResponse(res, req, http.StatusInternalServerError, "Error occured! keep calm")
 			return
 		} else {
+			plantName := req.Form.Get("name")
 			plantDesc := req.Form.Get("description") //* Get value from input description by form
-			res.Write([]byte(plantDesc))
+
+			temp, err := template.ParseFiles(path.Join("views", "plant.html"))
+			if err != nil {
+				errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
+				return
+			}
+
+			data := map[string]interface{}{
+				"ID": "dwdin93932in4239",
+				"Name": plantName,
+				"Description": plantDesc,
+			}
+
+			err = temp.Execute(res, data)
+			if err != nil {
+				errorResponse(res, req, http.StatusInternalServerError, "Render failed! keep calm")
+				return
+			}
 			return
 		}
 	} else {
-		errorResponse(res, req, http.StatusBadRequest, "Sorry! only GET method allowed by server!")
+		errorResponse(res, req, http.StatusBadRequest, "Sorry! only POST method allowed by server!")
 		return
 	}
 }
